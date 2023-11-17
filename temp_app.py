e699cfe19ac698ed46e838071296a8af134092df
Advaith Sanil Kumar <137145283+askadvaith@users.noreply.github.com>
2023-11-17 12:44:07 +0530

    Update temp_app.py

--- temp_app.py
+++ temp_app.py
@@ -1,10 +1,64 @@
 import streamlit as st
-# app.py
 
-st.title("My LLM-Based Application")
+# Function for LLM API interaction (dummy function for illustration)
+def generate_response(user_input):
+    # Replace this with your actual LLM API code
+    # Example: response = openai.Completion.create(model="text-davinci-002", prompt=user_input, ...)
+    # generated_text = response['choices'][0]['text']
+    # return generated_text
+    pass
 
-# User input components (e.g., text input, sliders)
-user_input = st.text_input("Enter your text:")
+# Streamlit app layout with improved styling
+def main():
+    # Set page configuration
+    st.set_page_config(
+        page_title="ChatGPT-Like UI with Streamlit",
+        page_icon=":speech_balloon:",
+        layout="wide",
+    )
 
-# Display outputs (e.g., results from the LLM)
-st.write("Output:")
+    # Set dark theme with shades of purple
+    st.markdown(
+        """
+        <style>
+            body {
+                background-color: #241b4f;
+                color: #ffffff;
+            }
+            .sidebar .sidebar-content {
+                background-color: #1a143a;
+            }
+            .css-1s0lmgv {
+                color: #c9c4d1;
+            }
+            .css-17eq0hr {
+                color: #ffffff;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    # Sidebar with dummy options
+    options = ["Option 1", "Option 2", "Option 3"]
+    selected_option = st.sidebar.selectbox("Select an Option", options)
+
+    # Main content for chat-like UI
+    st.title("ChatGPT-Like UI with Streamlit")
+
+    # Display user input and LLM-generated response in a chat-like format
+    user_input = st.text_input("You:", "")
+    
+    if st.button("Send"):
+        if user_input:
+            # Call LLM API function to generate a response
+            response = generate_response(user_input)
+
+            # Display user input and LLM-generated response in a chat-like format
+            st.text("")
+            st.text("User: " + user_input)
+            st.text("ChatGPT: " + response)
+
+if __name__ == "__main__":
+    main()
+
