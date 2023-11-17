b72f4870035919044aff87f8d53c828615b19603
Advaith Sanil Kumar <137145283+askadvaith@users.noreply.github.com>
2023-11-17 12:38:30 +0530

    temp_app

---
+++ temp_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+# app.py
+
+st.title("My LLM-Based Application")
+
+# User input components (e.g., text input, sliders)
+user_input = st.text_input("Enter your text:")
+
+# Display outputs (e.g., results from the LLM)
+st.write("Output:")
